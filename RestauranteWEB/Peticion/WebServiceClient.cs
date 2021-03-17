5fb802fcd54a9b7a63a866371bcd4494ef9d948d
szapeta <2375894860101@ingenieria.usac.edu.gt>
2021-03-17 05:51:50 -0600

    LLamada al servicio

---
+++ RestauranteWEB/Peticion/WebServiceClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace RestauranteWEB.Peticion
+{
+    public class WebServiceClient
+    {
+        public string WebServiceBaseUrl = "http://localhost:64963/";
+        public async Task<List<Tout>> GetListByIdWS<Tout>(string urlMetodoWithId)
+        {
+            List<Tout> respuesta = default(List<Tout>);
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(WebServiceBaseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(urlMetodoWithId);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        respuesta = await response.Content.ReadAsAsync<List<Tout>>();
+                    }
+                    else
+                    {
+                        var error = await response.Content.ReadAsStringAsync();
+                        throw new HttpRequestException(error);
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    // Handle exception.
+                    throw;
+                }
+                catch (Exception ex)
+                {
+                    // Handle exception.
+                    throw;
+                }
+            }
+
+            return respuesta;
+        }
+    }
+}
--- RestauranteWEB/Producto/ProductoLista.aspx.cs
+++ RestauranteWEB/Producto/ProductoLista.aspx.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using RestauranteMODEL.Model;
+using RestauranteWEB.Peticion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,6 +14,25 @@ namespace RestauranteWEB.Producto
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            obtenerDatosAsync();
+
+        }
+
+        public async Task<bool> obtenerDatosAsync()
+        {
+
+            try
+            {
+                WebServiceClient cliente = new WebServiceClient();
+                    var dato = cliente.GetListByIdWS<ProductoMDL>("api/producto/GetProductos");
+             
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
 
         }
     }
